1d271dfeb46382c2a5939dbdb63c8f5cc134f687
Vladimir Komissarov <dr0id@dr0id.ru>
2014-06-20 15:06:32 +0700

    Extends flattening example

--- examples/flattening.php
+++ examples/flattening.php
@@ -13,11 +13,21 @@ class User
 	 * @var Company
 	 */
 	public $company;
+	private $role;
 
-	public function __construct($name, Company $company)
+	public function __construct($name, Company $company, Role $role)
 	{
 		$this->name = $name;
 		$this->company = $company;
+		$this->role = $role;
+	}
+
+	/**
+	 * @return Role
+	 */
+	public function getRole()
+	{
+		return $this->role;
 	}
 }
 
@@ -31,14 +41,27 @@ class Company
 	}
 }
 
+class Role
+{
+	public $name;
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+}
+
 class UserDTO
 {
 	public $name;
 	public $companyName;
+	public $roleName;
 }
 
 /** @var UserDTO $userDTO */
-$userDTO = Papper::map(new User('John Smith', new Company('Acme Corporation')))->toType('Papper\Examples\Flattening\UserDTO');
+$userDTO = Papper::map(new User('John Smith', new Company('Acme Corporation'), new Role('Developer')))
+	->toType('Papper\Examples\Flattening\UserDTO');
 
 echo "Name: ", $userDTO->name, PHP_EOL;
 echo "Company name: ", $userDTO->companyName, PHP_EOL;
+echo "Role name: ", $userDTO->roleName, PHP_EOL;
